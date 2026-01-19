a10407bef3fb47b5af567225cac627a2d3e0e19d
Garbaun <189554890+Garbaun@users.noreply.github.com>
2026-01-20 00:20:21 +0300

    feat: update UI timings, spacing and text

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/SaaStraBCV/', // GitHub Pages için repo adı - Root deploy için kapalı
+  // base: '/', // GitHub Pages için repo adı - Root deploy için kapalı
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
